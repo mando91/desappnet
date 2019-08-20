bde1d6ac02c1fb6bc5cb9b5257a665c16b0546f0
Armando Rodarte Rodriguez <armandorodarte19@gmail.com>
2019-08-20 13:16:16 -0500

    holamundo

---
+++ 01.holamundo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01.holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // dotnet run -> para correr desde consola
+            Console.Clear();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Armando Rodarte Rodriguez");
+        }
+    }
+}

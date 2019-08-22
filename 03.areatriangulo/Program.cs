8570918446378422907d552008955b549364160e
Armando Rodarte Rodriguez <armandorodarte19@gmail.com>
2019-08-22 13:25:53 -0500

    programas

---
+++ 03.areatriangulo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _03.areatriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que calcula el area de un triangulo");
+
+            double area, baseT, altura;
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Forma de uso:");
+                Console.WriteLine("dotnet run <base> <altura>");
+               // Environment.Exit(0);
+            }else{
+                baseT = Double.Parse(args[0]);
+                altura = Double.Parse(args[1]);
+                area = (baseT * altura) /2;
+                Console.WriteLine($"el area es: {area}");
+            }
+        }
+    }
+}
---
+++ 04.pagatrabajador/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _04.pagatrabajador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string nombre;
+            double horas = 0, paga = 0, tasa = 0.3;
+            double pagaBruta = 0, pagaExtra = 0, impuesto = 0, pagaNeta  = 0;
+
+            Console.WriteLine("Programa que calcula la paga de un trabajador \n\n ");
+            
+            Console.WriteLine("Dame el nombre:");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Dame las horas que trabaja:");
+            horas = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dame la paga:");
+            paga = Double.Parse(Console.ReadLine());
+
+            if (horas > 40)
+            {
+                pagaExtra = (horas - 40) * (paga * 2);
+            }
+
+            pagaBruta = (horas- (horas - 40)) * paga;
+            pagaNeta = pagaBruta + pagaExtra;
+            impuesto = pagaBruta * tasa;
+            pagaNeta -= impuesto;
+            Console.WriteLine($"nombre: {nombre}, paga bruta {pagaBruta}, impuesto {impuesto}, paga neta {pagaNeta}, pagaExtra {pagaExtra}");
+        }
+    }
+}

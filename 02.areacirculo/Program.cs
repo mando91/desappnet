211007a8c7c973f2f917bb22578e15e9e7d06589
Armando Rodarte Rodriguez <armandorodarte19@gmail.com>
2019-08-22 12:34:14 -0500

    programada 2

---
+++ 02.areacirculo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02.areacirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            double radio = 0, area = 0; 
+
+            Console.Clear();
+            Console.WriteLine("Programa que calcula el area de un ciculo");
+            Console.WriteLine("Dame el radio");
+            radio = double.Parse(Console.ReadLine());
+            area = Math.PI + Math.Pow(radio,2);
+
+            Console.Write($"El area es {area}");
+        }
+    }
+}

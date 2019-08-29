800768c43b910322259756a62103d165d5e8bd02
Armando Rodarte Rodriguez <armandorodarte19@gmail.com>
2019-08-29 14:05:32 -0500

    programa 12

--- 12.vectorinverso/Program.cs
+++ 12.vectorinverso/Program.cs
@@ -6,7 +6,32 @@ namespace _12.vectorinverso
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double[] vectorA, vectorB;
+
+            vectorA = new double[15];
+            vectorB = new double[15];
+
+            Random rdm = new Random();
+
+            for (int i = 0; i < vectorA.Length; i++)
+            {
+                vectorA[i] = rdm.Next(1, 100);
+                vectorB[14-i] = vectorA[i];
+            }
+
+            Console.WriteLine("Vector original:");
+            imprimir(vectorA);
+            Console.WriteLine("Vector inverso:");
+            imprimir(vectorB);
+        }
+
+         static void imprimir(double[] vector)
+        {
+            for (int i = 0; i < vector.Length ; i++)
+            {
+                Console.Write(vector[i] + " ");
+            }
+            Console.WriteLine();
         }
     }
 }

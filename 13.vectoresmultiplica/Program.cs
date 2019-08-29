ccc2e582b62ebbbacd34a92a0c27d256a1b674b3
Armando Rodarte Rodriguez <armandorodarte19@gmail.com>
2019-08-29 13:41:30 -0500

    creacion de proyectos

---
+++ 13.vectoresmultiplica/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _13.vectoresmultiplica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

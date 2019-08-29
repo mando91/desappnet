e2bc5189f6418aff4a03603708c0d891fd4425d4
Armando Rodarte Rodriguez <armandorodarte19@gmail.com>
2019-08-29 12:28:16 -0500

    programa 8

---
+++ 08.vectorpromedio/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _08.vectorpromedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vector = {
+                10, 20, 30 , 40 ,50 ,60 ,70, 80, 90 ,100,
+                10, 20, 30 , 40 ,50 ,60 ,70, 80, 90 ,100,
+                10, 20, 30 , 40 ,50 ,60 ,70, 80, 90 ,100,
+                10, 20, 30 , 40 ,50 ,60 ,70, 80, 90 ,100,
+                10, 20, 30 , 40 ,50 ,60 ,70, 80, 90 ,100       
+            };
+
+            int sumea = 0, contNumMayoresProm = 0;
+            double prom = 0; 
+
+            Console.WriteLine("Los elementos del arreglo son:");
+            for (int i = 0; i < vector.Length; i++)
+            {
+                sumea += vector[i];
+                Console.Write(vector[i] + " ");
+            }
+
+            prom = sumea / vector.Length;
+
+            Console.WriteLine($"\n El promedio es:{prom}");
+
+             for (int i = 0; i < vector.Length; i++)
+            {
+                if (vector[i] > prom)
+                {
+                    contNumMayoresProm++;
+                }
+            }
+
+            Console.WriteLine($"\n La cantidad de numeros mayoes el prom es:{contNumMayoresProm}");
+        }
+    }
+}

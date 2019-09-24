613395db72103c0b65ad0a11677240023f95372d
Armando Rodarte Rodriguez <armandorodarte19@gmail.com>
2019-09-24 13:38:34 -0500

    17 program

---
+++ 17.MvcMovie/Models/MvcMovieContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace _17.MvcMovie.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}

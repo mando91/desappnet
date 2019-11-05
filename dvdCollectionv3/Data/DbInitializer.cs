9200999bcdc656f940c903f79a52799bc186954c
Armando Rodarte Rodriguez <armandorodarte19@gmail.com>
2019-11-05 13:18:27 -0600

    v4

--- dvdCollectionv3/Data/DbInitializer.cs
+++ dvdCollectionv3/Data/DbInitializer.cs
@@ -9,7 +9,7 @@ namespace dvdCollection.Data
             context.Database.EnsureCreated(); // crea bd si no existe
 
             if(context.FilmGeneres.Any()) {
-                return; // la bd ya tiene datos
+                 // la bd ya tiene datos
             }
 
             // arreglo del tipo FilmGeneres 
@@ -30,7 +30,44 @@ namespace dvdCollection.Data
                 context.FilmGeneres.Add(g);
             }
             //grabar los datos en la bd fisica
-            context.SaveChanges();      
+            context.SaveChanges();    
+
+            var FilmCertificates = new FilmCertificates[]{
+                new FilmCertificates{FilmCertificate="G"},
+                new FilmCertificates{FilmCertificate="PG"},
+                new FilmCertificates{FilmCertificate="R"},
+                new FilmCertificates{FilmCertificate="PG-13"}
+            }; 
+
+            foreach(FilmCertificates g in FilmCertificates) {
+                context.FilmCertificates.Add(g);
+            }
+            //grabar los datos en la bd fisica
+            context.SaveChanges();  
+
+            var Actors = new Actors[]{
+                new Actors {ActorFullName="Lianda Blair", ActorNotes="Actriz" },
+                new Actors {ActorFullName="Scarlett johansson", ActorNotes="Actriz" },
+                new Actors {ActorFullName="Pam", ActorNotes="Actriz" }
+            };
+
+            
+            foreach(Actors g in Actors) {
+                context.Actors.Add(g);
+            }
+            //grabar los datos en la bd fisica
+            context.SaveChanges(); 
+             var RoleTypes = new RoleTypes[] {
+                new RoleTypes {RoleDesc="Lead"},
+                new RoleTypes {RoleDesc="Principal"},
+                new RoleTypes {RoleDesc="Supporting"},
+                new RoleTypes {RoleDesc="Featured"},
+                new RoleTypes {RoleDesc="Extra"},
+            };
+            foreach(RoleTypes r in RoleTypes) {
+                context.RoleTypes.Add(r);
+            }
+            context.SaveChanges(); 
         }
     }
 }
